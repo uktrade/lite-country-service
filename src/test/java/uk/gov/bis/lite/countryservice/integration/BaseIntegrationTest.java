d3b0617ce64c3a96cd3896bf762a7ce9f943aaea
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-08-23 13:24:05 +0100

    WireMock configured to use dynamic port allocation

--- src/test/java/uk/gov/bis/lite/countryservice/integration/BaseIntegrationTest.java
+++ src/test/java/uk/gov/bis/lite/countryservice/integration/BaseIntegrationTest.java
@@ -1,15 +1,18 @@
 package uk.gov.bis.lite.countryservice.integration;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.containing;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static io.dropwizard.testing.FixtureHelpers.fixture;
 import static io.dropwizard.testing.ResourceHelpers.resourceFilePath;
 
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.testing.ConfigOverride;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.flywaydb.core.Flyway;
 import org.junit.Before;
@@ -21,24 +24,31 @@ import uk.gov.bis.lite.countryservice.config.CountryApplicationConfiguration;
 
 public class BaseIntegrationTest {
 
+  /**
+   * Configured to bind port dynamically to mitigate bug https://github.com/tomakehurst/wiremock/issues/97
+   * TODO revert to static port once bug is resolved
+   */
   @ClassRule
-  public static final WireMockRule wireMockRule = new WireMockRule(9000);
+  public static final WireMockClassRule wireMockClassRule = new WireMockClassRule(options().dynamicPort());
 
   @Rule
   public final DropwizardAppRule<CountryApplicationConfiguration> RULE =
-      new DropwizardAppRule<>(CountryServiceApplication.class, resourceFilePath("service-test.yaml"));
+      new DropwizardAppRule<>(CountryServiceApplication.class, resourceFilePath("service-test.yaml"),
+          ConfigOverride.config("spireClientUrl", "http://localhost:" +  wireMockClassRule.port() + "/spire/fox/ispire/"));
 
   @BeforeClass
   public static void setUp() throws Exception {
 
-    stubFor(post(urlEqualTo("/spirefox4dev/fox/ispire/SPIRE_COUNTRIES"))
+    configureFor(wireMockClassRule.port());
+
+    stubFor(post(urlEqualTo("/spire/fox/ispire/SPIRE_COUNTRIES"))
         .withRequestBody(containing("countrySetId"))
         .willReturn(aResponse()
             .withStatus(200)
             .withHeader("Content-Type", "text/xml")
             .withBody(fixture("spire-getCountrySet.xml"))));
 
-    stubFor(post(urlEqualTo("/spirefox4dev/fox/ispire/SPIRE_COUNTRIES"))
+    stubFor(post(urlEqualTo("/spire/fox/ispire/SPIRE_COUNTRIES"))
         .withRequestBody(containing("countryGroupId"))
         .willReturn(aResponse()
             .withStatus(200)
